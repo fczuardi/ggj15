5ad507d26972d35aa6d37f8443c45f854da09471
Fabricio C Zuardi <fabricio@fabricio.org>
2015-01-24 18:38:04 -0200

    adds watchify task to rebundle when watched files are modified

--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,12 @@ function onBundleError(e){
     console.log('BUNDLING ERROR!');
     console.log(e.name,e.fileName,'line:', e.lineNumber,'column:', e.column, e.description);
 }
+function bundleAppToDest(bundler){
+    return bundler.bundle()
+            .on('error', onBundleError)
+            .pipe(source(paths.dist.MAIN_JS_FILENAME))
+            .pipe(gulp.dest(paths.dist.JS));
+}
 var gulp = require('gulp'),
     browserify = require('browserify'),
     source = require('vinyl-source-stream'),
@@ -42,13 +48,28 @@ gulp.task('bundle-app-js', function(){
         packageCache: {},
         fullPaths: true
     };
-    return browserify(options)
-            .external(REQUIRED_LIBS)
-            .bundle()
+    var bundler = browserify(options).external(REQUIRED_LIBS);
+    return bundleAppToDest(bundler);
+});
+
+gulp.task('watch-bundle-app-js', function(){
+    var options = {
+        entries: [paths.MAIN_JS_ENTRY],
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    };
+    var watcher = watchify(browserify(options).external(REQUIRED_LIBS));
+    watcher.on('update',function(){
+        this.bundle()
             .on('error', onBundleError)
             .pipe(source(paths.dist.MAIN_JS_FILENAME))
             .pipe(gulp.dest(paths.dist.JS));
+        console.log('rebundled.');
+    });
+    return bundleAppToDest(watcher);
 });
+
 
 // Default task, is what the deployed version (ggj15.nulo.com.br) runs
 gulp.task('default',
