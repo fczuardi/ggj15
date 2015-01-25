4bf98a73515521d1896696d10adb7e102af4281b
Fabricio C Zuardi <fabricio@fabricio.org>
2015-01-25 01:35:04 -0200

    reorganizes code, attach entities from playerB to a container and rotates 180 degree

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 var Crafty = require('craftyjs');
 
+var c = require('../../constants').game;
+
 require('./components/floor');
 require('./components/avatar');
 require('./components/playableAvatar');
@@ -7,52 +9,57 @@ require('./components/balloon');
 require('./components/portal');
 require('./components/fps');
 
-var c = require('../../constants').game;
-
 Crafty.init(c.WIDTH, c.HEIGHT);
 Crafty.background('#cce');
 
-Crafty.e('FloorA, Floor')
-    .attr({
-        y: c.HEIGHT - c.FLOOR_HEIGHT
-    });
 
-Crafty.e('PlayerA, PlayableAvatar')
-    .attr({
-        x: 0,
-        y: c.HEIGHT / 2
-    })
-    .color('red')
-    .gravityConst(c.GRAVITY)
-    .gravity('FloorA');
+var world = Crafty.e('2D,Canvas'),
+    screenA = Crafty.e('2D, Canvas')
+                .attr({
+                    x:0,
+                    y:c.HEIGHT/2,
+                    w:c.WIDTH,
+                    h:c.HEIGHT/2
+                })
+                .origin('center'),
+    screenB = Crafty.e('2D, Canvas')
+                .attr({
+                    x:0,
+                    y:0,
+                    w:c.WIDTH,
+                    h:c.HEIGHT/2
+                })
+                .origin('center'),
+    floorA = Crafty.e('Floor')
+                .attr({
+                    y: c.HEIGHT - c.FLOOR_HEIGHT
+                }),
+    avatarA = Crafty.e('PlayableAvatar')
+                .attr({
+                    x: 0,
+                    y: floorA.y - c.PLAYER_HEIGHT
+                }),
+    floorB = Crafty.e('Floor')
+                .attr({
+                    y: c.HEIGHT/2 - c.FLOOR_HEIGHT
+                }),
+    avatarB = Crafty.e('Avatar')
+                .attr({
+                    x: 0,
+                    y: floorB.y - c.PLAYER_HEIGHT
+                }),
+    portal = Crafty.e('Portal'),
+    balloon = Crafty.e('Balloon')
+                .attr({
+                    x: 100,
+                    y: floorA.y - c.BALLOON_HEIGHT
+                })
+                .color('yellow');
 
-Crafty.e('BalloonA, Balloon')
-    .attr({
-        x: 100,
-        y: c.HEIGHT / 2
-    })
-    .color('yellow')
-    .gravityConst(c.BALLOON_GRAVITY)
-    .gravity('FloorA');
-
-//-----
-
-Crafty.e('FloorB, Floor')
-    .attr({
-        y: 0
-    });
-
-Crafty.e('PlayerB, Avatar')
-    .attr({
-        x: 0,
-        y: c.HEIGHT / 2 - c.PLAYER_HEIGHT
-    })
-    .color('darkgray')
-    .gravityConst(c.GRAVITY * -1)
-    .gravity('FloorB');
-
-//----- Portal -------
-
-Crafty.e('MainPortal, Portal');
+avatarA.color('red');
+avatarB.color('darkgray');
+screenA.attach(floorA, avatarA, balloon);
+screenB.attach(floorB, avatarB).attr({rotation:180});
+world.attach(screenA, screenB).attr({rotation:0});
 
 Crafty.e('FPSCounter, Fps');
